4b6b7e0ed5d62fc2ea9a885170ae6f867dded96d
nickbreaton <nick@breaton.com>
2017-05-09 16:29:04 -0400

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const { resolve } = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env) => {
+  return {
+    devtool: env.dev ? 'source-map' : undefined,
+    entry: [
+      resolve(__dirname, 'src/scripts/index.js'),
+      resolve(__dirname, 'src/styles/index.scss')
+    ],
+    output: {
+      filename: 'index.js',
+      path: resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        { test: /\.json$/, use: 'json-loader' },
+        { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'public' }
+      ]),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        minify: {
+          collapseWhitespace: env.prod,
+          removeComments: env.prod
+        },
+        template: 'src/index.html'
+      })
+    ]
+  }
+}
