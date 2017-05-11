1a6f862ad0c91e88ef259797f21b8e3e37a65f0a
nickbreaton <nick@breaton.com>
2017-05-11 01:49:26 -0400

    Add loading spinner

--- src/scripts/_load.js
+++ src/scripts/_load.js
@@ -3,10 +3,8 @@ import $ from 'jquery';
 let image = $('#cover').css('background-image');
 image = image.replace(/.*(?=http)/, '');
 image = image.replace(/[^A-Za-z]*$/, '');
-console.log(image);
 
 $('<img/>').attr('src', image).on('load', function () {
   $(this).remove();
-  $('body').css('visibility', 'visible');
-  console.log('done');
+  $('section').css('visibility', 'visible');
 });
