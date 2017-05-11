533c4431b6c2097b7d38d040b2c70270495e6afa
nickbreaton <nick@breaton.com>
2017-05-11 11:51:17 -0400

    Dynamically update favicon

--- src/scripts/_cover.js
+++ src/scripts/_cover.js
@@ -1,11 +1,19 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
+  // set initial title
   document.title = 'Dear Mom';
+
+  // set favicon as heart
+  $('link[rel="shortcut icon"]').attr('href', './favicon-heart.ico');
 });
 
 $('#cta').click(() => {
+  // change title
   document.title = 'Tina Breaton | Web Developer';
+
+  // set favicon to code
+  $('link[rel="shortcut icon"]').attr('href', './favicon-code.ico');
 
   // start css lift animation
   $('#cover').addClass('lift');
